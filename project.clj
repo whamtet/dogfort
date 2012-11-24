971c0ce8d2ebc5cc197b3a7567ee2e825979a712
Bodil Stokke <bodil@bodil.tv>
2012-11-24 21:34:36 +0100

    Add plugin for Node REPL.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject dogfort "0.1.0-SNAPSHOT"
+(defproject org.bodil/dogfort "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-cljsbuild "0.2.9"]]
+  :plugins [[lein-cljsbuild "0.2.9"]
+            [org.bodil/lein-noderepl "0.1.1"]]
   :cljsbuild {:builds
               [{:source-path "src"
                 :compiler

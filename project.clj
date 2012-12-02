269d217f7000d575e84a7bbfc0fe6f87d78d0ad4
Bodil Stokke <bodil@bodil.tv>
2012-12-02 03:10:55 +0100

    Set a proper license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.bodil/dogfort "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.bodil/redlobster "0.1.0"]
                  [hiccups "0.1.1"]]

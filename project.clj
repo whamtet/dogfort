b8f5ab4fe0b8cd1b94701fd2d8e5830da5a71e2c
Bodil Stokke <bodil@bodil.tv>
2012-12-23 13:48:58 +0100

    Insert missing project description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.bodil/dogfort "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A web server framework for Clojurescript on Node"
+  :url "https://github.com/bodil/dogfort"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]

0604f3ca79c6436dff8bfa7c5673f1e932ab5f1e
Matthew Molloy <whamtet@gmail.com>
2019-02-11 18:00:39 +0700

    updated versions

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject dogfort "0.2.1"
+(defproject dogfort "0.2.2-SNAPSHOT"
   :description "A web server framework for Clojurescript on Node"
   :url "https://github.com/bodil/dogfort"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [redlobster "0.2.2-SNAPSHOT"]
+                 [redlobster "0.2.2"]
                  [org.clojure/clojurescript "1.7.48"]
                  [org.clojure/tools.nrepl "0.2.10"]
                  [org.clojars.whamtet/hiccups "0.4.0-SNAPSHOT"]]
